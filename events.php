8319cb7df7a00191a999d9f5d2e3cf559eb22e66
nishantprajapati123 <knishant147@gmail.com>
2019-01-06 20:16:16 +0530

    Events categories pages is added

---
+++ events.php
@@ -0,0 +1,298 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  	<meta http-equiv="x-ua-compatible" content="ie=edge">
+  	<title>NITR Utsav</title>
+  	<!-- Font Awesome -->
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  	<!-- Bootstrap core CSS -->
+  	<link href="https://mdbootstrap.com/previews/templates/landing-page/css/bootstrap.min.css" rel="stylesheet">
+  	<!-- Material Design Bootstrap -->
+  	<link href="https://mdbootstrap.com/previews/templates/landing-page/css/mdb.min.css" rel="stylesheet">
+  	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+  
+	<style>
+    	html,
+        body,
+        header,
+        .jarallax {
+          height: 100%;
+        }
+
+        @media (min-width: 560px) and (max-width: 740px) {
+          html,
+          body,
+          header,
+          .jarallax {
+            height: 500px;
+          }
+        }
+
+        @media (min-width: 800px) and (max-width: 850px) {
+            .navbar:not(.top-nav-collapse) {
+                background: #3f51b5!important;
+            }
+            .navbar {
+              box-shadow: 0 2px 5px 0 rgba(0,0,0,.16), 0 2px 10px 0 rgba(0,0,0,.12) !important;
+            }
+        }
+
+        .font-small {
+            font-size: 0.9rem;
+        }
+    </style>
+    
+	<script>document.documentElement.className="js";var supportsCssVars=function(){var e,t=document.createElement("style");return t.innerHTML="root: { --tmp-var: bold; }",document.head.appendChild(t),e=!!(window.CSS&&window.CSS.supports&&window.CSS.supports("font-weight","var(--tmp-var)")),t.parentNode.removeChild(t),e};supportsCssVars()||alert("Please view this demo in a modern browser that supports CSS Variables.");</script>
+</head>
+
+<body class="event-lp">
+  	<!-- Navigation & Intro -->
+    	<!-- Navbar -->
+    	<nav class="navbar navbar-expand-lg navbar-dark fixed-top top-nav-collapse" style="background-color: #cc0000">
+      		<div class="container">
+        		<a class="navbar-brand" href="#">NITR Utsav</a>
+        		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          			<span class="navbar-toggler-icon"></span>
+        		</button>
+        
+				<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          			<ul class="navbar-nav ml-auto smooth-scroll">
+            			<li class="nav-item">
+              				<a class="nav-link" href="#home">Campus Ambassador Registration</a>
+            			</li>
+          			</ul>
+        		</div>
+      		</div>
+    	</nav>
+	<!-- Main content -->
+  	<main>
+    	<div class="container" id="about">
+      		<!-- Section: Features v.1 -->
+      		<section id="features" class="section feature-box mt-5 mb-5 text-center">
+
+        		<!-- Section heading -->
+        		<h3 class="text-center dark-grey-text font-weight-bold mb-5 mt-5 pt-5 wow fadeIn" data-wow-delay="0.2s">
+          			<strong>EVENTS</strong>
+        		</h3>
+
+        		<!-- Section description -->
+        		<div class="row">
+        			<div class="col-lg-4 col-md-6 col-sm-12 ">
+        				<div class="image animated rollIn delay-1s" style="opacity: 0;">
+        						<a href="#">
+        							<img src="icons/xCompetitions.png.pagespeed.ic.aToADul0tm.webp" alt="" class="img-responsive icon-size" data-pagespeed-url-hash="991711873">
+        							<p class="icon-words">Competitions</p>
+        						</a>
+        		
+        					</div>
+        				</div>
+        			<div class="col-lg-4 col-md-6 col-sm-12 ">
+        				<div class="image animated rollIn delay-1s" style="opacity: 0;">
+        					<a href="#">
+        						<img src="icons/xExhibitions.png.pagespeed.ic.Y8SO8J0sW3.webp" alt="" class="img-responsive icon-size" data-pagespeed-url-hash="991711873">
+        						<p class="icon-words">Fun Events</p>
+        					</a>
+        				</div>
+        			</div>
+        			<div class="col-lg-4 col-md-6 col-sm-12 ">
+        				<div class="image animated rollIn delay-1s" style="opacity: 0;">
+        					<a href="#">
+        						<img src="icons/xFlagship.png.pagespeed.ic.HljCAhYebn.webp" alt="" class="img-responsive icon-size" data-pagespeed-url-hash="991711873">
+        						<p class="icon-words">Flagships</p>
+        					</a>
+        				
+        				</div>
+        			</div>
+        			<div class="col-lg-4 col-md-6 col-sm-12 ">
+        				<div class="image animated rollIn delay-1s" style="opacity: 0;">
+        				<div class="image">
+        					<a href="#">
+        						<img src="icons/xMusical,P20Night.png.pagespeed.ic.NFqw8BdY7F.webp" alt="" class="img-responsive icon-size" data-pagespeed-url-hash="991711873">
+        						<p class="icon-words">Musical Night</p>
+        					</a>
+        				</div>
+        			</div>
+        			</div>
+        			<div class="col-lg-4 col-md-6 col-sm-12 ">
+        				<div class="image animated rollIn delay-1s" style="opacity: 0;">
+        				<div class="image">
+        					<a href="#">
+        						<img src="icons/xProshows.png.pagespeed.ic.D0CWp-lfuL.webp" alt="" class="img-responsive icon-size" data-pagespeed-url-hash="991711873">
+        						<p class="icon-words">Proshows</p>
+        					</a>
+        				</div>
+        			</div>
+        			</div>
+        			<div class="col-lg-4 col-md-6 col-sm-12 ">
+        				<div class="image animated rollIn delay-1s" style="opacity: 0;">
+        				<div class="image">
+        					<a href="#">
+        						<img src="icons/xWorkshops.png.pagespeed.ic.0UW73YiNxo.webp" alt="" class="img-responsive icon-size" data-pagespeed-url-hash="991711873">
+        						<p class="icon-words">Workshops, Exhibitions & Guest Talks</p>
+        					</a>
+        				</div>
+        				</div>
+        			</div>
+        			
+        		</div>
+        		<!--Grid row-->
+      		</section>
+    	</div>
+  	</main>
+  	<!-- Main content -->
+	
+  	<!--Footer-->
+  	<footer class="page-footer indigo text-center text-md-left pt-0">
+    	<!--Footer Links-->
+    	<div class="container">
+      		<!--Grid row-->
+      		<div class="row pt-5 mb-3 text-center d-flex justify-content-center">
+				<!--Grid column-->
+				<div class="col-md-2 mb-3">
+					<h6 class="text-uppercase font-weight-bold">
+						<a href="#!">About us</a>
+					</h6>
+				</div>
+				<!--Grid column-->
+
+				<!--Grid column-->
+				<div class="col-md-2 mb-3">
+					<h6 class="text-uppercase font-weight-bold">
+						<a href="#!">Products</a>
+					</h6>
+				</div>
+				<!--Grid column-->
+
+				<!--Grid column-->
+				<div class="col-md-2 mb-3">
+					<h6 class="text-uppercase font-weight-bold">
+						<a href="#!">Awards</a>
+					</h6>
+				</div>
+				<!--Grid column-->
+
+				<!--Grid column-->
+				<div class="col-md-2 mb-3">
+					<h6 class="text-uppercase font-weight-bold">
+						<a href="#!">Help</a>
+					</h6>
+				</div>
+				<!--Grid column-->
+
+				<!--Grid column-->
+				<div class="col-md-2 mb-3">
+					<h6 class="text-uppercase font-weight-bold">
+						<a href="#!">Contact</a>
+					</h6>
+				</div>
+				<!--Grid column-->
+      		</div>
+      		<!--Grid row-->
+
+			<hr class="rgba-white-light" style="margin: 0 15%;">
+
+			<!--Grid row-->
+			<div class="row d-flex text-center justify-content-center mb-md-0 mb-4">
+				<!--Grid column-->
+				<div class="col-md-8 col-12 mt-5">
+					<p style="line-height: 1.7rem">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+				doloremque laudantium, totam
+				rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+				explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+				consequuntur.</p>
+				</div>
+				<!--Grid column-->
+			</div>
+			<!--Grid row-->
+
+      		<hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
+
+			<!--Grid row-->
+			<div class="row pb-3">
+				<!--Grid column-->
+				<div class="col-md-12">
+					<div class="footer-socials mb-5 flex-center">
+						<!--Facebook-->
+						<a class="p-2 m-2 fa-lg fb-ic">
+							<i class="fa fa-facebook fa-lg white-text mr-md-4"> </i>
+						</a>
+						<!--Twitter-->
+						<a class="p-2 m-2 fa-lg tw-ic">
+							<i class="fa fa-twitter fa-lg white-text mr-md-4"> </i>
+						</a>
+						<!--Google +-->
+						<a class="p-2 m-2 fa-lg gplus-ic">
+							<i class="fa fa-google-plus fa-lg white-text mr-md-4"> </i>
+						</a>
+						<!--Linkedin-->
+						<a class="p-2 m-2 fa-lg li-ic">
+							<i class="fa fa-linkedin fa-lg white-text mr-md-4"> </i>
+						</a>
+						<!--Instagram-->
+						<a class="p-2 m-2 fa-lg ins-ic">
+							<i class="fa fa-instagram fa-lg white-text mr-md-4"> </i>
+						</a>
+						<!--Pinterest-->
+						<a class="p-2 m-2 fa-lg pin-ic">
+							<i class="fa fa-pinterest fa-lg white-text"> </i>
+						</a>
+					</div>
+				</div>
+				<!--Grid column-->
+			</div>
+			<!--Grid row-->
+		</div>
+    	<!--/Footer Links-->
+
+		<!--Copyright-->
+		<div class="footer-copyright py-3 text-center">
+		  	<div class="container-fluid">
+				© 2018 Copyright: <a href="https://nitrutsav.com/" target="_blank"> NITR Utsav </a>
+		  	</div>
+		</div>
+		<!--/Copyright-->
+  	</footer>
+  	<!--/Footer-->
+  
+	<!--  JQuery  -->
+  	<script type="text/javascript" src="https://mdbootstrap.com/previews/templates/landing-page/js/jquery-3.3.1.min.js"></script>
+	<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/1.13.1/TweenMax.min.js'></script>
+  	<!--  Bootstrap tooltips  -->
+  	<script type="text/javascript" src="https://mdbootstrap.com/previews/templates/landing-page/js/popper.min.js"></script>
+  	<!--  Bootstrap core JavaScript  -->
+  	<script type="text/javascript" src="https://mdbootstrap.com/previews/templates/landing-page/js/bootstrap.min.js"></script>
+  	<!--  MDB core JavaScript  -->
+  	<script type="text/javascript" src="https://mdbootstrap.com/previews/templates/landing-page/js/mdb.min.js"></script>
+	<script type="text/javascript" src="js/index.js"></script>
+	<script type="text/javascript" src="js/imagesloaded.pkgd.min.js"></script>
+	<script type="text/javascript" src="js/masonry.pkgd.min.js"></script>
+	<script type="text/javascript" src="js/charming.min.js"></script>
+<!--	<script type="text/javascript" src="js/TweenMax.min.js"></script>-->
+	<script type="text/javascript" src="js/demo.js"></script>
+  	<script>
+		//Animation init
+		new WOW().init();
+		
+		//Modal
+		$('#myModal').on('shown.bs.modal', function () {
+		  $('#myInput').focus()
+		})
+		
+		// MDB Lightbox Init
+		$(function () {
+		  $("#mdb-lightbox-ui").load("https://mdbootstrap.com/previews/templates/landing-page/mdb-addons/mdb-lightbox-ui.html");
+		});
+  	</script>
+	<script>
+		// Load app.js script after document has rendered.
+		var script = document.createElement('script');
+		script.src = 'js/app.js';
+		script.type = 'text/javascript';
+		document.getElementsByTagName('body')[0].appendChild(script);
+	</script>
+</body>
+</html>
